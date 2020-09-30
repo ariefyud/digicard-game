bb4418b8db71b2691a7a12258204d725ac865269
ariefyud <ariefyud12@gmail.com>
2020-09-30 19:25:26 +0700

    Updates

---
+++ precache-manifest.37d2c0ffa881daff0eb1847011ebcf96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5999d266a0ed82df01ba4f633cd8a9bf",
+    "url": "/digicard-game/index.html"
+  },
+  {
+    "revision": "efff4361e02b18eab802",
+    "url": "/digicard-game/static/css/main.4b24cd35.chunk.css"
+  },
+  {
+    "revision": "fbf4ffbeb889b42a7cfe",
+    "url": "/digicard-game/static/js/2.dc3c3321.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/digicard-game/static/js/2.dc3c3321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efff4361e02b18eab802",
+    "url": "/digicard-game/static/js/main.52628050.chunk.js"
+  },
+  {
+    "revision": "260c4a7d8b535c7474d6",
+    "url": "/digicard-game/static/js/runtime-main.e8ab3067.js"
+  }
+]);
